2aaf231c4a63a768e5ade4e55c17e5930c0ef3fd
jakaria242 <jakaria.dev242@gmail.com>
2024-01-26 21:50:29 +0600

    navbar design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,23 +8,39 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Image from '../../utils/Image';
 import gmail from '../../assets/images/gmail.png'
 import '../../utils/image.css'
+import { Avatar, Grid } from '@mui/material';
+import Search from '../../assets/images/search.png'
+import '../../utils/image.css'
+import {auth} from '../../config/FirebaseConfig.jsx'
 
 const Navbar = () => {
   return (
-    <>
-     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{backgroundColor:'#f9f9f9'}}>
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2, color:'#3c3c3c' }}>
+    <Grid container>
+      <Box sx={{ flexGrow: 1 }}>
+       <AppBar elevation={0} position="static" sx={{backgroundColor:'#f9f9f9', height:"5vw", paddingTop:"7px"}}>
+       <Toolbar variant="dense">
+        <Grid item xs={2}>
+          <div style={{display:'flex', alignItems:'center'}}>
+           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2, color:'#3c3c3c' }}>
             <MenuIcon/>
-          </IconButton>
-          <Image style="img" sorce={gmail} alter="Not Found"/>
-          <Typography sx={{color:'#3c3c3c', marginLeft:'10px', fontWeight:'500'}} variant="h6" component="div">
-            Gmail
-          </Typography>
+           </IconButton>
+           <Image style="img" sorce={gmail} alter="Not Found"/>
+           <Typography sx={{color:'#3c3c3c', marginLeft:'10px', fontWeight:'500'}} variant="h6" component="div">Gmail</Typography>
+          </div>
+        </Grid>
+        <Grid item xs={9}>
+          <div style={{borderRadius:"40px", backgroundColor:'#E4EFFA', display:"flex", alignItems:"center", width:"60vw", height:"3.7vw"}}>
+          <Image style="search" sorce={Search} alter="Not Found"/>
+          <input placeholder='Search mail' style={{width:'55vw', height:'3vw', backgroundColor:'#E4EFFA',border:'none',}}/>
+          </div>
+        </Grid>
+        <Grid item xs={1}>
+        <Avatar sx={{marginLeft:"70px"}} src={auth.currentUser?.photoURL}/>
+        </Grid>
         </Toolbar>
       </AppBar>
-    </Box></>
+      </Box>
+    </Grid>
   )
 }
 
